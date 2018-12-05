afcd92f0b1bebd6d009dd0327bf0be64d20b68e4
LYJr <lyi91@naver.com>
2018-12-05 16:36:18 +0900

    json step5

--- src/main/java/category/blog/BlogController.java
+++ src/main/java/category/blog/BlogController.java
@@ -1,6 +1,8 @@
 package category.blog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/blogs")
 public class BlogController {
 
-    @GetMapping("")
-    public String blog(){
-        return "/category/blog";
-    }
+    @Autowired
+    private BlogRepository blogRepository;
+
 }
