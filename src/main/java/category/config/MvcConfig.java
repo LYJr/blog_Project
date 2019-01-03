e2b004a893734217585b37ada4244557a550240e
LYJr <lyi91@naver.com>
2019-01-03 16:04:35 +0900

    url distribution

--- src/main/java/category/config/MvcConfig.java
+++ src/main/java/category/config/MvcConfig.java
@@ -12,13 +12,13 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/blogs").setViewName("/category/blog");
-        registry.addViewController("/abouts").setViewName("/category/about");
-        registry.addViewController("/features").setViewName("/category/features");
-        registry.addViewController("/portfolio-list").setViewName("/category/portfolio");
-        registry.addViewController("/user").setViewName("/category/user");
-        registry.addViewController("/single").setViewName("/category/portfolio-single");
-        registry.addViewController("/form").setViewName("/category/login_form");
-        registry.addViewController("/login").setViewName("/category/login");
+        registry.addViewController("/blogs").setViewName("category/blog");
+        registry.addViewController("/abouts").setViewName("category/about");
+        registry.addViewController("/features").setViewName("category/features");
+        registry.addViewController("/portfolio-list").setViewName("category/portfolio");
+        registry.addViewController("/user").setViewName("category/user");
+        registry.addViewController("/single").setViewName("category/portfolio-single");
+        registry.addViewController("/form").setViewName("category/login_form");
+        registry.addViewController("/login").setViewName("category/login");
     }
 }
